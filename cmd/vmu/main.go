1fa3d2601fb46f2b398b667dfbeabd395a8a4f34
brian <brianjipson@pm.me>
2025-05-26 15:12:48 -0400

    Add NFO file parsing for episode matching in main
    Integrate `nfo.MatchEpisodeFile` and `nfo.ParseEpisodeNFO` to enable parsing and logging of episode metadata. Errors during matching or parsing are logged, and episode details are displayed for verification.

--- cmd/vmu/main.go
+++ cmd/vmu/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/rs/zerolog/log"
 	"go-vmu/internal/config"
 	"go-vmu/internal/logger"
+	"go-vmu/internal/nfo"
 )
 
 func main() {
@@ -15,5 +16,19 @@ func main() {
 
 	log.Info().Str("startup", "logger").Msg("Logger started")
 	log.Info().Msgf("Config: %v", cfg)
+
+	testPath := "/mnt/eagle5/videos/Tv/Westworld/Season 1/Westworld - S01E01 - The Original Bluray-1080p.mkv"
+
+	nfoPath, err := nfo.MatchEpisodeFile(testPath)
+	if err != nil {
+		log.Error().Err(err).Msg("Error matching NFO file")
+	}
+	data, err := nfo.ParseEpisodeNFO(nfoPath)
+	if err != nil {
+		log.Error().Err(err).Msg("Error parsing NFO file")
+	}
+	log.Info().Msgf("NFO path: %s\n", nfoPath)
+	log.Info().Msgf("NFO: %v\n", data)
+	log.Info().Msgf("Episode Title: %s\n", data.Title)
 
 }
