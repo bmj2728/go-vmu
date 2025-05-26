59033c2dd6a22dd1788427794f205a7ffe661034
brian <brianjipson@pm.me>
2025-05-26 15:04:46 -0400

    Add NFO parsing and episode matching functionality
    Introduced functionality to parse `.nfo` files into structured `EpisodeDetails`. Added methods to locate and validate NFO files corresponding to episode paths, enhancing NFO file handling and structured episode metadata extraction.

--- internal/nfo/nfo.go
+++ internal/nfo/nfo.go
@@ -1,1 +1,90 @@
 package nfo
+
+import "encoding/xml"
+
+type EpisodeDetails struct {
+	XMLName   xml.Name `xml:"episodedetails"`
+	Plot      string   `xml:"plot"`
+	LockData  bool     `xml:"lockdata"`
+	DateAdded string   `xml:"dateadded"`
+	Title     string   `xml:"title"`
+	Director  []string `xml:"director"`
+	Writer    string   `xml:"writer"`
+	Credits   string   `xml:"credits"`
+	Rating    float64  `xml:"rating"`
+	Year      int      `xml:"year"`
+	MPAA      string   `xml:"mpaa,omitempty"`
+	IMDBID    string   `xml:"imdbid"`
+	TVDBID    string   `xml:"tvdbid"`
+	Runtime   int      `xml:"runtime"`
+	Genre     []string `xml:"genre"`
+	Art       Art      `xml:"art"`
+	Actor     []Actor  `xml:"actor"`
+	ShowTitle string   `xml:"showtitle"`
+	Episode   int      `xml:"episode"`
+	Season    int      `xml:"season"`
+	Aired     string   `xml:"aired"`
+	FileInfo  FileInfo `xml:"fileinfo"`
+}
+
+type Art struct {
+	Poster string `xml:"poster"`
+}
+
+type Actor struct {
+	Name      string `xml:"name"`
+	Role      string `xml:"role"`
+	Type      string `xml:"type"`
+	SortOrder int    `xml:"sortorder"`
+	Thumb     string `xml:"thumb,omitempty"`
+}
+
+type FileInfo struct {
+	StreamDetails StreamDetails `xml:"streamdetails"`
+}
+
+type StreamDetails struct {
+	Video    []VideoStream    `xml:"video"`
+	Audio    []AudioStream    `xml:"audio"`
+	Subtitle []SubtitleStream `xml:"subtitle"`
+}
+
+type VideoStream struct {
+	Codec             string  `xml:"codec"`
+	MiCodec           string  `xml:"micodec"`
+	Bitrate           int     `xml:"bitrate"`
+	Width             int     `xml:"width"`
+	Height            int     `xml:"height"`
+	Aspect            string  `xml:"aspect"`
+	AspectRatio       string  `xml:"aspectratio"`
+	Framerate         float64 `xml:"framerate"`
+	Language          string  `xml:"language,omitempty"`
+	ScanType          string  `xml:"scantype"`
+	Default           bool    `xml:"default"`
+	Forced            bool    `xml:"forced"`
+	Duration          int     `xml:"duration"`
+	DurationInSeconds int     `xml:"durationinseconds"`
+}
+
+type AudioStream struct {
+	Codec        string `xml:"codec"`
+	MiCodec      string `xml:"micodec"`
+	Bitrate      int    `xml:"bitrate"`
+	Language     string `xml:"language"`
+	ScanType     string `xml:"scantype"`
+	Channels     int    `xml:"channels"`
+	SamplingRate int    `xml:"samplingrate"`
+	Default      bool   `xml:"default"`
+	Forced       bool   `xml:"forced"`
+}
+
+type SubtitleStream struct {
+	Codec    string `xml:"codec"`
+	MiCodec  string `xml:"micodec"`
+	Width    int    `xml:"width"`
+	Height   int    `xml:"height"`
+	Language string `xml:"language"`
+	ScanType string `xml:"scantype"`
+	Default  bool   `xml:"default"`
+	Forced   bool   `xml:"forced"`
+}
--- internal/nfo/parser.go
+++ internal/nfo/parser.go
@@ -1,1 +1,69 @@
 package nfo
+
+import (
+	"encoding/xml"
+	"fmt"
+	"github.com/rs/zerolog/log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// NFONotFoundError represents an error message when an NFO file cannot be located.
+// NFOReadError represents an error message for issues reading an NFO file.
+// NFOUnMarshallError represents an error message when unmarshalling an NFO file fails.
+const (
+	NFONotFoundError   = "error locating nfo file"
+	NFOReadError       = "error reading nfo file"
+	NFOUnMarshallError = "error unmarshalling nfo file"
+)
+
+// ParseEpisodeNFO parses the given NFO file path into an EpisodeDetails struct.
+// Returns an error if the file cannot be opened, read, or unmarshalled into the struct.
+func ParseEpisodeNFO(path string) (*EpisodeDetails, error) {
+	//We're testing the validity of the nfo file path in the matching step
+	log.Debug().Strs("nfo", []string{path, "loading"}).Msg("Parsing NFO file")
+	//Open the file
+	file, err := os.Open(path)
+	if err != nil {
+		log.Error().Err(err).Msg(NFOReadError)
+		return nil, fmt.Errorf(NFOReadError+": %v", err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Error().Err(err).Msg(NFOReadError)
+		}
+	}(file)
+	//Parse the xml using std lib
+	details := &EpisodeDetails{}
+	decoder := xml.NewDecoder(file)
+	if err := decoder.Decode(details); err != nil {
+		log.Error().Err(err).Msg(NFOUnMarshallError)
+		return nil, fmt.Errorf(NFOUnMarshallError+": %v", err)
+	}
+	return details, nil
+}
+
+// MatchEpisodeFile attempts to find an NFO file corresponding to the given episode file path.
+// It checks both the existence of the given file and the deduced NFO file in the same directory.
+// Returns the path to the located NFO file or an error if it does not exist.
+func MatchEpisodeFile(path string) (string, error) {
+	log.Debug().Strs("nfo", []string{path, "matching"}).Msg("Matching NFO file")
+	//validate existence or just give up
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		log.Error().Err(err).Msg(NFONotFoundError)
+		return "", fmt.Errorf(NFONotFoundError+": %v", err)
+	}
+	//get working directory + name
+	wd := filepath.Dir(path)
+	base := strings.Split(filepath.Base(path), ".")[0]
+	nfoPath := wd + "/" + base + ".nfo"
+	log.Debug().Strs("nfo", []string{path, "working directory " + wd, "base " + base, "expected " + nfoPath}).Msg("Working directory")
+	//Test our guess
+	if _, err := os.Stat(nfoPath); os.IsNotExist(err) {
+		log.Error().Err(err).Msg(NFONotFoundError)
+		return "", fmt.Errorf(NFONotFoundError+": %v", err)
+	}
+	return nfoPath, nil
+}
