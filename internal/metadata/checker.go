87958ca0db92b9adf9c6df7db1f94a918a3a341b
Brian Jipson <brian.jipson@novelgit.com>
2025-06-15 13:07:02 -0400

    Enhance metadata comparison with normalization and detailed logging in `MetaChecker` and `worker`.

--- internal/metadata/checker.go
+++ internal/metadata/checker.go
@@ -1,5 +1,11 @@
 package metadata
 
+import (
+	"fmt"
+	"github.com/rs/zerolog/log"
+	"strings"
+)
+
 type Checker interface {
 	Compare() bool
 }
@@ -18,12 +24,23 @@ func NewMetaChecker(existing map[string]interface{}, compare map[string]interfac
 
 func (m *MetaChecker) Compare() bool {
 	//compare is new data since we want this to be the data we check each value
+	//normalize the data
+	normalizedExisting := make(map[string]interface{})
+	for k, v := range m.ExistingMetadata {
+		normalizedExisting[strings.ToUpper(k)] = fmt.Sprintf("%v", v)
+	}
+	normalizedCompare := make(map[string]interface{})
 	for k, v := range m.CompareMetadata {
-		//if the key-value is not found or doesn't match we return
-		if m.ExistingMetadata[k] != v {
+		normalizedCompare[strings.ToUpper(k)] = fmt.Sprintf("%v", v)
+	}
+
+	for k, v := range normalizedCompare {
+		if normalizedExisting[k] != v {
+			log.Info().Msgf("Inconsistency found: %s - Old:%v New:%v", k, m.ExistingMetadata[k], v)
 			return false
 		}
 	}
 	//if map processes without a false return true
+	log.Info().Msg("No inconsistencies found - skipping file")
 	return true
 }
